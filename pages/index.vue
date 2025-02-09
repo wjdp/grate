301e4ecf16af164a52808eac47f899024b696230
Will Pimblett <will@wjdp.uk>
2025-02-09 21:37:40 +0000

    Play

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const appConfig = useAppConfig()
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <p>Mornin {{ appConfig.title }}</p>
+  </div>
+</template>
---
+++ pages/state.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+const a = useState('a', ()=> 5);
+const b = useState('b', ()=> 10);
+const incrementB = () => b.value++;
+const resetB = () => b.value = 0;
+</script>
+
+<template>
+{{ a }} + {{ b }} = {{ a + b }}
+<button @click="incrementB">Increment b</button>
+<button @click="resetB">Reset b</button>
+</template>

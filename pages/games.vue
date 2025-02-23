46700b0b2ba20095921ee4b9286ea11bc5de1123
Will Pimblett <will@wjdp.uk>
2025-02-23 19:32:42 +0000

    Add UI games list (hella basic)

---
+++ pages/games.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const {data} = useFetch(() => '/api/games')
+</script>
+
+<template>
+  <div>
+    <h1>Games</h1>
+    <ul v-if='data'>
+        <li v-for='game in data.games' :key='game.id'>
+            <NuxtLink :to='`/games/${game.id}`'>{{ game.name }}</NuxtLink>
+        </li>
+    </ul>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 const appConfig = useAppConfig()
+
+const {data} = useFetch(() => '/api/setup')
 </script>
 
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <p>Mornin {{ appConfig.title }}</p>
+    <p v-if="data && data.user">grate is configured</p>
+    <p v-if="data && !data.user">grate needs setup</p>
     <p><NuxtLink to="/state">State</NuxtLink></p>
+    <p><NuxtLink to="/games">Games</NuxtLink></p>
   </div>
 </template>

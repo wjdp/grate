6bbdadeac779f32f8abcd5ba7e05f0821c13c2bf
Will Pimblett <will@wjdp.uk>
2025-02-26 23:24:05 +0000

    Implement a client and debug page

---
+++ composables/useSseClient.ts
@@ -0,0 +1,43 @@
+import type { SseMessageMap, SseMessageType } from "~/lib/hooks"
+
+let eventSource: EventSource
+
+export const useSseClient = () => {
+    if (import.meta.server) {
+        return {
+            onMessage: <T extends SseMessageType>(name: T, callback: (data: SseMessageMap[T]) => void) => { },
+            open: ref(false),
+            close: () => { }
+        }
+    }
+
+    const reconnect = () => {
+        if (eventSource) {
+            eventSource.close()
+        }
+        eventSource = new EventSource("/api/sse")
+        eventSource.onerror = (event) => {
+            console.error("EventSource error:", event)
+            eventSource.close()
+            setTimeout(reconnect, 1000)
+        }
+    }
+
+    reconnect();
+    const open = ref(eventSource.readyState === EventSource.OPEN);
+    eventSource.onopen = () => open.value = true
+
+    return {
+        // 
+        onMessage: <T extends SseMessageType>(name: T, callback: (data: SseMessageMap[T]) => void) => {
+            eventSource.addEventListener(name, (event) => {
+                const message = JSON.parse(event.data);
+                callback(message)
+            })
+        },
+        open,
+        close: () => {
+            eventSource.close()
+        }
+    }
+}
---
+++ pages/debug/sse.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+    const { onMessage, open } = useSseClient();
+    const messages = ref<string[]>([]);
+    onMessage('message', (event) => {
+        messages.value.push(`Message: ${event.message}`);
+    });
+    onMessage('notification', (event) => {
+        messages.value.push(`${event.title}: ${event.message}`);
+    });
+</script>
+
+<template>
+    <div>
+        <h1>Server-Sent Events</h1>
+        <p v-if="open">We're open</p>
+        <p v-else>Not open</p>
+        <ul>
+            <li v-for="message in messages" :key="message">{{ message }}</li>
+        </ul>
+    </div>
+</template>

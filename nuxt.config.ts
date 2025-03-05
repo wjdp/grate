3dc1dc62b10ca3f0fe5961c5e9ac2f40f1abd074
Will Pimblett <will@wjdp.uk>
2025-03-05 21:38:10 +0000

    Install nuxt-trpc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@prisma/nuxt', 'nuxt-cron'],
+  build: {
+    transpile: ['trpc-nuxt'],
+  },
   nitro: {
     plugins: ['bigint.ts']
   },

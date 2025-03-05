6b11191c4a6d37b3ea6c5940048792bafb8aa5d9
Will Pimblett <will@wjdp.uk>
2025-03-05 21:38:48 +0000

    Implement a basic TRPC endpoint and client

---
+++ pages/debug/trpc.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { $client } = useNuxtApp()
+
+const { data: hello } = await $client.hello.useQuery({ text: 'abc'})
+</script>
+
+<template>
+  <div>
+    <h1>Debug trpc</h1>
+    <p>{{ hello?.greeting }}</p>
+  </div>
+</template>

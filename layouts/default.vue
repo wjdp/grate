61d8767536320ed067c8d0d96a6b97aae6687bbc
Will Pimblett <will@wjdp.uk>
2025-03-06 02:05:27 +0000

    Change one small thing

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen bg-slate-900 text-white p-4">
+    <div class="min-h-screen bg-slate-900 text-white p-2">
       <header>
         <NuxtLink to="/" class='font-bold'>grate</NuxtLink> | <NuxtLink to="/games">Games</NuxtLink>
       </header>

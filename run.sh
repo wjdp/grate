1a5a79a863ede280caa3c07feb35bc742012d5cd
Will Pimblett <will@wjdp.uk>
2025-03-07 02:00:03 +0000

    Correct the migrate command used in prod
    db push is very wrong
    See https://www.prisma.io/docs/orm/prisma-migrate/workflows/prototyping-your-schema#choosing-db-push-or-prisma-migrate

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Migrate the db and start the server
 # Used in the docker container
-node_modules/.bin/prisma db push
+node_modules/.bin/prisma migrate deploy
 node .output/server/index.mjs

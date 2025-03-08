421faf723a1ed147cf84ee453abd55c41f58a2d3
Will Pimblett <will@wjdp.uk>
2025-03-08 18:34:12 +0000

    Add vitest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@prisma/nuxt", "nuxt-cron", "@nuxtjs/tailwindcss"],
+  modules: [
+    "@prisma/nuxt",
+    "nuxt-cron",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/test-utils/module",
+  ],
   build: {
     transpile: ["trpc-nuxt"],
   },

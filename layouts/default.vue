2ac41b226a274131dd3bab2060efe670c9129ec4
Will Pimblett <will@wjdp.uk>
2025-03-15 20:33:56 +0000

    Improve header styling
    Still basic

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,19 @@
+<script setup lang="ts">
+const links = {
+  Games: "/games",
+  Organise: "/organise",
+  Tasks: "/debug/tasks",
+};
+</script>
+
 <template>
   <div class="min-h-screen bg-slate-900 p-2 text-white">
     <header>
-      <NuxtLink to="/" class="font-bold">grate</NuxtLink> |
-      <NuxtLink to="/games">Games</NuxtLink> |
-      <NuxtLink to="/organise">Organise</NuxtLink>
+      <NuxtLink to="/" class="mr-2 font-bold">grate</NuxtLink>
+      <template v-for="(link, name, index) in links" :key="name">
+        <span class="opacity-50">|</span>
+        <NuxtLink :to="link" class="mx-2">{{ name }}</NuxtLink>
+      </template>
     </header>
     <slot />
   </div>

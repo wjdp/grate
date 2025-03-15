2f8dbcf388528f030b1c7e741af4efc33cb98937
Will Pimblett <will@wjdp.uk>
2025-03-15 18:54:31 +0000

    Also cache icons

---
+++ server/steam/icon.ts
@@ -0,0 +1,26 @@
+import { ART_DIR } from "~/server/constants";
+import { checkFileExists } from "~/server/files";
+import { SteamGame } from "@prisma/client";
+import fs from "fs";
+
+function getFilePathForIcon(appId: number) {
+  return `${ART_DIR}/steam/${appId}/icon.jpg`;
+}
+
+export async function isSteamIconCached(appId: number): Promise<boolean> {
+  return await checkFileExists(getFilePathForIcon(appId));
+}
+
+function getSteamIconUrl(steamGame: SteamGame) {
+  return `http://media.steampowered.com/steamcommunity/public/images/apps/${steamGame.appId}/${steamGame.imgIconUrl}.jpg`;
+}
+
+export async function cacheSteamIconForApp(steamGame: SteamGame) {
+  const iconUrl = getSteamIconUrl(steamGame);
+  const filePath = getFilePathForIcon(steamGame.appId);
+  const fileDirectory = filePath.split("/").slice(0, -1).join("/");
+  const artResponse = await fetch(iconUrl);
+  const buffer = Buffer.from(await artResponse.arrayBuffer());
+  await fs.promises.mkdir(fileDirectory, { recursive: true });
+  await fs.promises.writeFile(filePath, buffer);
+}
--- server/tasks/queueable/cacheSteamArt.ts
+++ server/tasks/queueable/cacheSteamArt.ts
@@ -1,5 +1,6 @@
 import { cacheSteamArtForApp, isSteamArtCached } from "~/server/steam/art";
 import prisma from "~/lib/prisma";
+import { cacheSteamIconForApp, isSteamIconCached } from "~/server/steam/icon";
 
 async function cacheArtForSingleGame(appId: number) {
   const isCached = await isSteamArtCached(appId);
@@ -9,9 +10,18 @@ async function cacheArtForSingleGame(appId: number) {
   }
 }
 
+async function cacheIconForSingleGame(steamGame: SteamGame) {
+  const isCached = await isSteamIconCached(steamGame.appId);
+  if (!isCached) {
+    console.log(`Caching steam icon for app ${steamGame.appId}`);
+    await cacheSteamIconForApp(steamGame);
+  }
+}
+
 export default async function cacheSteamArt() {
   const steamGames = await prisma.steamGame.findMany();
   for (const game of steamGames) {
-    await cacheArtForSingleGame(game.appId as number);
+    // await cacheArtForSingleGame(game.appId as number);
+    await cacheIconForSingleGame(game);
   }
 }

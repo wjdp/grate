843d10231aa03efd39fc8f9a12453b19d11d5d01
Will Pimblett <will@wjdp.uk>
2025-03-16 23:41:24 +0000

    Replace @nuxtjs/tailwindcss with Tailwind v4
    - Fixes issues I was having with the tailwind IDE plugin
    - Moves tailwind config into CSS
    - Uses vite plugin directly rather than nuxt wrapper, one less layer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: [
     "@prisma/nuxt",
     "nuxt-cron",
-    "@nuxtjs/tailwindcss",
     "@nuxt/test-utils/module",
     "@nuxt/fonts",
   ],
+  css: ["~/assets/css/main.css"],
   build: {
     transpile: ["trpc-nuxt"],
   },
@@ -17,6 +19,7 @@ export default defineNuxtConfig({
     experimental: { tasks: true },
   },
   vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
         // https://github.com/nuxt/nuxt/issues/24690#issuecomment-2254528534
